4ea4164165ccec98d66cf99b473f86488f1d9a82
Mirandalaw <74170593+Mirandalaw@users.noreply.github.com>
2021-04-05 15:38:08 +0900

    Add files via upload

---
+++ 1463(1로 만들기).cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+int main()
+{
+	int X;
+	cin >> X;
+	vector<int> dp(X+1);
+
+	for (int i = 2; i <= X; ++i)
+	{
+		dp[i] = dp[i - 1] + 1;
+		if (i % 2 == 0)dp[i] = min(dp[i / 2] + 1, dp[i]);
+		if (i % 3 == 0)dp[i] = min(dp[i / 3] + 1, dp[i]);
+	}
+	cout << dp[X];
+	return 0;
+}
