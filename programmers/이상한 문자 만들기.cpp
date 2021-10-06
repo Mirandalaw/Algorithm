78e133c7245d50c71f9e15565ddd344f58fb81bd
JeongPark <74170593+Mirandalaw@users.noreply.github.com>
2021-10-06 21:52:20 +0900

    Create 이상한 문자 만들기.cpp

---
+++ programmers/이상한 문자 만들기.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    vector<int> siz;
+    for(int i=0;i<s.size();++i){
+        if(s[i]==' ')siz.push_back(i);
+    }
+    siz.push_back(s.size());
+    int n=0;
+    for(int j=0;j<siz.size();++j){
+        int cnt=0;
+        for(int i=n;i<siz[j];++i){
+            if(cnt%2==0&&s[i]>96&&s[i]<=122)s[i]-=32;//대문자로 
+            else if(cnt%2!=0&&s[i]>64&&s[i]<=90)s[i]+=32;//소문자로
+            cnt++;
+        }
+        n=siz[j]+1;
+    }
+    return s;
+}
