6411f94cb6f761dfc377a8603e2ea3351e1b7ad1
JeongPark <74170593+Mirandalaw@users.noreply.github.com>
2021-10-23 22:55:16 +0900

    Create 15650(N과 M(2)).cpp

---
+++ baejoon/15650(N과 M(2)).cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#define MAX 9
+using namespace std;
+int N, M;
+int arr[MAX];
+int visited[MAX];
+void dfs(int cnt,int current) {
+	if (cnt == M) {
+		for (int i = 0; i < M; ++i) {
+			cout << arr[i]<<" ";
+		}
+		cout << "\n";
+		return;
+	}
+	for (int i = current; i <= N; ++i) {
+		if (!visited[i]) {
+			visited[i] = 1;
+			arr[cnt] = i;
+			dfs(cnt + 1, i);
+			visited[i] = 0;
+		}
+	}
+}
+int main() {
+	cin >> N >> M;
+	dfs(0,1);
+	return 0;
+}
