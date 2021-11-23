d2768ca2f694ddea0af310b9456b0725f5f831bc
JeongPark <74170593+Mirandalaw@users.noreply.github.com>
2021-11-24 02:56:05 +0900

    Math Method

---
+++ programmers/Level 1/가운데 글자 가져오기.js
@@ -0,0 +1,17 @@
+function solution(s) {
+    var answer = '';
+    var si = s.length;
+    if(si%2===0){
+        si/=2;
+        answer=s[si-1]+s[si];
+    }
+    else {
+        var num= Math.floor(si/=2);
+        answer=s[num];
+    }
+    return answer;
+}
+/**다른 사람의 풀이*/
+function solution(s) {
+  return s.substr(Math.ceil(s.length/2)-1, s.length % 2 ===0 ? 2:1);
+}
