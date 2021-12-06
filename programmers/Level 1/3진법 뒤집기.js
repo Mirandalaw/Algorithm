df041772616aa6dc5288432793ba53b5382981ca
JeongPark <74170593+Mirandalaw@users.noreply.github.com>
2021-12-06 23:30:54 +0900

    월간 코드 챌린지 시즌1

---
+++ programmers/Level 1/3진법 뒤집기.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    let answer = 0;
+    let b=[]
+    while(n!=0){
+        b.push(n%3);
+        n=Math.floor(n/3)
+    }
+    console.log(b);
+    for(let i=b.length-1,j=0;i>=0;--i){
+        let a= Math.pow(3,j);
+        answer+=(b[i]*a);
+        j++;
+    }
+    return answer;
+}
