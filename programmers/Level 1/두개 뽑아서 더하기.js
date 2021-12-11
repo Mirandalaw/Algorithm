95d47939528f53c2e58f2ac3d16e92bf30c051f2
JeongPark <74170593+Mirandalaw@users.noreply.github.com>
2021-12-11 21:02:13 +0900

    월간 코드 챌린지 시즌 1

--- programmers/Level 1/두개 뽑아서 더하기.js
+++ programmers/Level 1/두개 뽑아서 더하기.js
@@ -1,13 +1,19 @@
 function solution(numbers) {
-    let answer= [];
-    let set = new Set();
+    var answer = [];
     for(let i=0;i<numbers.length;++i){
         for(let j=i+1;j<numbers.length;++j){
             answer.push(numbers[i]+numbers[j]);
         }
     }
-    
-    set.add(answer);
-    console.log(set);
-    return answer;
+    //*******************************
+    const set = new Set(answer);
+    const uniqueArr = [...set];
+    //*******************************
+    //=> 중복값이 있는 배열을 Set 객체로 만들어서 중복을 제거한뒤
+    //Spread Operator(전개연산자)를 사용하여 Set 객체를 다시 배열로 변환
+    //Spread Operator 대신 Array.from() 또는 forEach() 문 사용가능
+    uniqueArr.sort((a,b)=>{
+        return a-b;
+    })
+    return uniqueArr;
 }
