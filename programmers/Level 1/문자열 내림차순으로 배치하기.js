3411562d4ebca7c076c3c48faaa50a7f355c8c26
JeongPark <74170593+Mirandalaw@users.noreply.github.com>
2022-02-14 19:52:52 +0900

    Update 문자열 내림차순으로 배치하기.js

--- programmers/Level 1/문자열 내림차순으로 배치하기.js
+++ programmers/Level 1/문자열 내림차순으로 배치하기.js
@@ -1,3 +1,9 @@
 function solution(s) {
     return s.split("").sort().reverse().join("");
 }
+/*
+    split()을 통해 문자열을 잘라 배열로 만듭니다.
+    sort()는 유니코드 순으로 정렬해줍니다.
+    reverse()는 배열을 내림차순으로 변환합니다.
+    join()을 통해 배열을 합쳐 다시 문자열로 변경해줍니다.
+*/
