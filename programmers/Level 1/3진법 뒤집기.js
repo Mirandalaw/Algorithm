75f13974175ec7e6c0a1b01d5abbbe954d7dd45d
JeongPark <74170593+Mirandalaw@users.noreply.github.com>
2022-03-18 23:12:12 +0900

    Update 3진법 뒤집기.js

--- programmers/Level 1/3진법 뒤집기.js
+++ programmers/Level 1/3진법 뒤집기.js
@@ -5,11 +5,14 @@ function solution(n) {
         b.push(n%3);
         n=Math.floor(n/3)
     }
+    
     console.log(b);
+    
     for(let i=b.length-1,j=0;i>=0;--i){
         let a= Math.pow(3,j);
         answer+=(b[i]*a);
         j++;
     }
+    
     return answer;
 }
