e4aa399018d170da03910d2cea6cf4c7be88ba78
JeongPark <74170593+Mirandalaw@users.noreply.github.com>
2022-05-09 23:09:04 +0900

    Update 체육복.js

--- programmers/Level 1/체육복.js
+++ programmers/Level 1/체육복.js
@@ -17,3 +17,29 @@ function solution(n, lost, reserve) {
     }
     return answer;
 }
+
+function solution(n, lost, reserve) {
+    var answer = 0;
+    let arr= new Array(n).fill(1);
+    for(let i=0;i<lost.length;++i){
+        arr[lost[i]-1]--;
+    }
+    for(let i=0;i<reserve.length;++i){
+        arr[reserve[i]-1]++;
+    }
+    for(let i=0;i<n;++i){
+        if(arr[i]==0){
+            if(arr[i-1]==2){
+                arr[i]++;
+                arr[i-1]--;
+            }
+            else if(arr[i+1]==2){
+                arr[i]++;
+                arr[i+1]--;
+            }
+        }
+        if(arr[i]>=1)answer++;
+    }
+   
+    return answer;
+}
