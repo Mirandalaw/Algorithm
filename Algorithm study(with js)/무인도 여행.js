a2d2e3f8d116d26c110758e0e9ac8b76bee57c2e
JeongPark <74170593+Mirandalaw@users.noreply.github.com>
2023-01-31 00:08:52 +0900

    히든케이스 해결

--- Algorithm study(with js)/무인도 여행.js
+++ Algorithm study(with js)/무인도 여행.js
@@ -3,23 +3,20 @@ function solution(maps) {
     let map = initMap(maps);
     if(map ==-1) return [-1];
     let visited = initVisited(maps);
-    let dx = [0,1,0,-1];
-    let dy = [-1,0,1,0];
     
+    let dx = [1,0,-1,0];
+    let dy = [0,1,0,-1];
   
     for(let col=0;col<maps.length;++col){
-        let sum = 0;
         for(let row =0;row<maps[col].length;++row){
             if(!visited[col][row]){
-                let result = bfs(row,col,visited,map,dx,dy,sum);
+                let result = bfs(row,col,visited,map,dx,dy,map[col][row]);
                 if(result>0) answer.push(result);
             }
         }
     }
-     
     return answer.sort((a,b)=> a-b);
 }
-
 
 function bfs(x,y,visited,map,dx,dy,sum){
     let queue = [];
@@ -29,23 +26,22 @@ function bfs(x,y,visited,map,dx,dy,sum){
         let q = queue.shift();
         let x = q[0];
         let y = q[1];
+        
         for(let dir = 0;dir<4;++dir){
             let nx = x+dx[dir];
             let ny = y+dy[dir];
-            if(nx>=0&&nx<map[0].length&&ny>=0&&ny<map.length){
+            
+            if(nx>=0&&nx<map[0].length&&ny>=0&&ny<map.length&&map[y][x]!=-1){
                 if(!visited[ny][nx]&&map[ny][nx]!=-1){
-                    queue.push([nx,ny]);
-                    visited[ny][nx]=true;
                     sum+=map[ny][nx];
+                    map[ny][nx]+=map[y][x];
+                    visited[ny][nx]=true;
+                    queue.push([nx,ny]);
                 }
             }
         }
     }
-    console.log(sum);
-    if(sum==0){
-        return -1;
-    }
-    else return sum;
+    return sum;   
 }
 
 function initMap(maps){
