907658fc6fb91b0245c59560fbff66d8f54b0232
JeongPark <74170593+Mirandalaw@users.noreply.github.com>
2025-05-23 18:40:18 +0900

    백준 12738 - LIS O(N log N) 풀이 (12015 패턴 강화)

---
+++ baejoon/binary_search/12738_가장 긴 증가하는 부분 수열 3.js
@@ -0,0 +1,44 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+    input.push(line);
+}).on("close", function () {
+
+    const N = Number(input[0]);
+    const A = input[1].split(' ').map(Number);
+    solve(A);
+    process.exit();
+});
+
+const lowerBound = (arr, target) => {
+    let left = 0;
+    let right = arr.length;
+
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+        if (arr[mid] >= target) right = mid;
+        else left = mid + 1;
+    }
+    return left;
+};
+
+const solve = (A) => {
+    let LIS = [];
+
+    for (const num of A) {
+        let idx = lowerBound(LIS, num)
+        if (idx === LIS.length) {
+            LIS.push(num);
+        } else {
+            LIS[idx] = num;
+        }
+    }
+    console.log(LIS.length);
+}
